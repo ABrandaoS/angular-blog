723f1a289dd9d63c43cc1af63fddebd25d256634
Alexandre <abrandaos@outlook.com>
2025-07-22 20:03:38 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id": 1,
+        "image":"https://legadodamarvel.com.br/wp-content/uploads/2025/07/Quarteto-Fantastico-Marvel-libera-os-primeiros-5-minutos-do-filme-legadodamarvel.webp",
+        "title": "Quarteto Fantástico: Marvel libera os primeiros 5 minutos do filme",
+        "description": "A estreia de Quarteto Fantástico: Primeiros Passos está cada vez mais próxima, e a Marvel Studios decidiu dar um gostinho especial para quem acompanhou a première mundial do filme no Disney+. Após o final do evento, que aconteceu nesta segunda-feira (22), a Marvel exibiu os primeiros 5 minutos do longa. Revelando detalhes importantes sobre a origem da equipe no MCU."
+    },
+    {
+        "id": 2,
+        "image":"https://legadodamarvel.com.br/wp-content/uploads/2025/07/Marvel-oficializa-o-diretor-do-filme-dos-X-Men-no-MCU-legadodamarvel.webp",
+        "title": "Marvel oficializa o diretor do filme dos X-Men no MCU",
+        "description": "Após meses de rumores, o Presidente da Marvel Studios, Kevin Feige confirmou quem será o diretor do filme dos X-Men no MCU. Depois de tantos rumores, teorias e especulações que dominaram a internet nos últimos meses, a confirmação finalmente chegou. Em entrevista ao ComicBook.com, o presidente da Marvel Studios, Kevin Feige, anunciou oficialmente que Jake Schreier, diretor de Thunderbolts*, será o responsável por comandar o aguardado filme dos X-Men no MCU. O filme dos X-Men no MCU vem aí! Ainda sem data de estreia definida, o novo filme dos X-Men já está em desenvolvimento e marca o início de uma nova fase no Universo Cinematográfico da Marvel. Kevin Feige garantiu que o roteiro já está sendo escrito, e destacou o quanto a experiência com Jake Schreier em Thunderbolts* foi determinante para essa escolha. “É oficial, Jake Schreier vai dirigir X-Men para a gente, e somos muito, muito sortudos. […] Está tudo começando agora”, afirmou o chefão da Marvel. Thunderbolts* foi a chave para a escolha. Mesmo com desempenho abaixo nas bilheterias, Thunderbolts* ainda assim conquistou fãs e crítica de forma surpreendente, sendo considerado um dos projetos mais coesos da fase atual da Marvel Studios."
+    },
+    {
+        "id": 3,
+        "image":"https://legadodamarvel.com.br/wp-content/uploads/2025/05/Quando-Thunderbolts-sera-lancado-no-Disney-legadodamarvel.webp",
+        "title": "Quando Thunderbolts* será lançado no Disney+?",
+        "description": "Thunderbolts* está em exibição nos cinemas, mas quando o filme irá estrear no Disney+?"
+    },
+    {
+        "id": 4,
+        "image":"https://legadodamarvel.com.br/wp-content/uploads/2025/07/Vingadores-Doomsday-trara-o-inacreditavel-retorno-deste-Vingador-original-legadodamarvel.webp",
+        "title": "Vingadores: Doomsday trará o inacreditável retorno deste Vingador original",
+        "description": "Após desfecho de seu arco em Vingadores: Ultimato, este Vingador original tem retorno confirmado em Doomsday. A saga do Capitão América parecia ter chegado ao fim em Vingadores: Ultimato, quando Steve Rogers decidiu devolver as Joias do Infinito e viver uma vida ao lado de Peggy Carter. Mas isso aparentemente está prestes a mudar. O insider MyTimeToShineHello garantiu neste domingo (20) que Chris Evans retornará ao papel de Steve Rogers em Vingadores: Doomsday. E ainda segundo o insider, o retorno não será apenas uma participação especial, e sim com Steve Rogers tendo um papel fundamental no filme, sendo essencial para o desenvolvimento da trama. E ainda segundo o insider, o retorno não será apenas uma participação especial, e sim com Steve Rogers tendo um papel fundamental no filme, sendo essencial para o desenvolvimento da trama."
+    }
+]
